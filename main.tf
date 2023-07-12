ee69b9eed2c3c2f02ae6340f3da227d89ccaf7d7
sahil1384 <sahil.techtweekinfotech@gmail.com>
2023-07-12 16:20:37 +0530

    firstsg

--- main.tf
+++ main.tf
@@ -28,3 +28,29 @@ resource "aws_vpc" "sahil8113" {
     Name = "testing-terraform"
   }
 }
+
+resource "aws_security_group" "sahil_sg" {
+  name        = "sahil-security-group"
+  description = "for testing"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

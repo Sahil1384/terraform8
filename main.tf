fc06d2c15cd6e5a3ba48205c2295344b5622fe9b
sahil1384 <sahil.techtweekinfotech@gmail.com>
2023-07-25 16:09:18 +0530

    sahil

--- main.tf
+++ main.tf
@@ -180,7 +180,7 @@ resource "aws_instance" "terraform32" {
   instance_type = var.instance-type  # Replace with your desired instance type
   subnet_id     = aws_subnet.terraform23.id
   key_name      = var.key-name
-  security_group_id = aws_security_group.terraform_grp.id
+  aws_security_group = aws_security_group.terraform_grp.id
    tags = {
     Name = "terrafor32"  # Change this to the desired instance name
   }

91072c84867a8f32332d589aacd8124f8dd4cad9
sahil1384 <sahil.techtweekinfotech@gmail.com>
2023-07-27 15:03:05 +0530

    infrastructure

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_vpc" "vpc" {
 # aws subnet creation
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.terraform.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.0.0/24"
 
   tags = {

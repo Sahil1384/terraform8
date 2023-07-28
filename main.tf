5a8c41e4b5f0e325ba5e98589c355fa8941207dd
sahil1384 <sahil.techtweekinfotech@gmail.com>
2023-07-28 15:36:01 +0530

    lb

--- main.tf
+++ main.tf
@@ -35,10 +35,7 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "public-subnet" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-  ]
+  cidr_block = var.cidr_block
 
   tags = {
     Name = "public-subnet"
--- variable.tf
+++ variable.tf
@@ -13,3 +13,10 @@ variable "aws_subnet" {
   default = "public-subnet"
 }
 
+variable "cidr_block" {
+  type = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+}
